ed5dff796d9a381003838060df07541021a9f5d1
Zhaolong Wang <1896216@johnabbottcollege.net>
2019-09-14 15:34:36 -0400

    update

--- Exercise/WebHitCounter/src/main/java/main/WebHitCounterMain.java
+++ Exercise/WebHitCounter/src/main/java/main/WebHitCounterMain.java
@@ -1,6 +1,7 @@
 package main;
 
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
@@ -12,11 +13,23 @@ public class WebHitCounterMain {
     public static void main(String[] args) throws Exception {
         Configuration conf = new Configuration();
         Job job = Job.getInstance(conf, "Daily Web Hit Counter");
-        job.setJarByClass(main.WebHitCounterMain.class);
+        Path output = new Path(args[1]);
+
+        FileSystem hdfs = FileSystem.get(conf);
+
+        if (hdfs.exists(output)) {
+            hdfs.delete(output, true);
+        }
+
+        job.setJarByClass(WebHitCounterMain.class);
         job.setMapperClass(mapper.WebHitCounterMapper.class);
         job.setReducerClass(reducer.WebHitCounterReducer.class);
+        job.setCombinerClass(reducer.WebHitCounterReducer.class);
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(IntWritable.class);
+        job.setMaxMapAttempts(4);
+        job.setMaxReduceAttempts(2);
+        job.setNumReduceTasks(3);
         FileInputFormat.addInputPath(job, new Path(args[0]));
         FileOutputFormat.setOutputPath(job, new Path(args[1]));
         System.exit(job.waitForCompletion(true) ? 0 : 1);
